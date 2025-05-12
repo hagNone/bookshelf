415c2596896ff129b655b6f1523118b58b43d338
Manoj kr <161196049+hagNone@users.noreply.github.com>
2025-05-12 16:19:16 +0530

    sdv

--- bookswap/urls.py
+++ bookswap/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyOTPView , LoginView, Userprofile #, SendResetOTPView, ResetPasswordView,SearchBooksAPIView,BookDetailAPIView,user_logout,post_request
+from .views import RegisterView, VerifyOTPView , LoginView, Userprofile,SearchBooksAPIView,BookDetailAPIView#, SendResetOTPView, ResetPasswordView,SearchBooksAPIView,BookDetailAPIView,user_logout,post_request
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,8 +7,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
 #     path('send-reset-otp/', SendResetOTPView.as_view(), name='send_reset_otp'),
 #     path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
-#     path('search/', SearchBooksAPIView.as_view(), name='search-books-page'),
-#     path('book/<int:pk>/', BookDetailAPIView.as_view(), name='book-detail-page'),
+    path('search/', SearchBooksAPIView.as_view(), name='search-books-page'),
+    path('book/<int:pk>/', BookDetailAPIView.as_view(), name='book-detail-page'),
     path('profile/',Userprofile.as_view(),name='profile-page'),
 #     path("logout/",user_logout,name="user-Logout"),
 #     path('upload_book/',post_request,name='upload_book')
